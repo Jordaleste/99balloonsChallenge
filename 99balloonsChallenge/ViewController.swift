1aded1a7c60cd5eca6e0a686731032b2b4468afd
Jordaleste <charlie@esnenterprises.com>
2015-01-26 14:44:40 -0500

    First Commit

--- 99balloonsChallenge/ViewController.swift
+++ 99balloonsChallenge/ViewController.swift
@@ -9,17 +9,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var countLabel: UILabel!
+    
+    var balloonInstance: [Balloon] = []
+    var currentIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.countLabel.text = ""
+        self.myImageView.image = UIImage(named: "BerlinTVTower.jpg")
+    
+        self.balloonCreat()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
 
 
+    @IBAction func nextButtonPressed(sender: UIBarButtonItem) {
+    
+    
+        
+        let balloon = balloonInstance[currentIndex]
+        countLabel.text = "\(balloon.count) balloons"
+        myImageView.image = balloon.image
+        
+        currentIndex += 1
+        
+        
+    }
+    
+    func balloonCreat () {
+        for var balloonCount = 0; balloonCount <= 99; balloonCount++ {
+            var balloon = Balloon()
+            balloon.count = balloonCount
+            
+            let randomNumber = Int(arc4random_uniform(UInt32(4)))
+            
+            switch randomNumber {
+            case 1:
+                balloon.image = UIImage(named: "RedBalloon1.jpg")
+            case 2:
+                balloon.image = UIImage(named: "RedBalloon2.jpg")
+            case 3:
+                balloon.image = UIImage(named: "RedBalloon3.jpg")
+            default:
+                balloon.image = UIImage(named: "RedBalloon4.jpg")
+                
+            }
+            self.balloonInstance.append(balloon)
+            
+        }
+    }
+    
 }
 
---
+++ Balloon.swift
@@ -0,0 +1,15 @@
+//
+//  Balloon.swift
+//  99balloonsChallenge
+//
+//  Created by Charles Eison on 1/26/15.
+//  Copyright (c) 2015 Charles Eison. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Balloon {
+    var count = 0
+    var image = UIImage(named: "")
+}
